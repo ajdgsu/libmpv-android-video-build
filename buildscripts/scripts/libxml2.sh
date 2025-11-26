0c4a0306919a067f3daa7be4ce4845f77b3c9fa8
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-11-27 00:36:44 +0800

    Update libxml2.sh

--- buildscripts/scripts/libxml2.sh
+++ buildscripts/scripts/libxml2.sh
@@ -20,6 +20,9 @@ cd _build$ndk_suffix
 export ac_cv_header_sys_timeb_h=no
 
     CFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" CXXFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" \
+	echo -e "\n-----------------\configure help start\n-----------------\n"
+	../configure --help
+	echo -e "\n-----------------\configure help end\n-----------------\n"
 	../configure \
 	--host=aarch64-linux-android \
 	--build=x86_64-pc-linux-gnu \
