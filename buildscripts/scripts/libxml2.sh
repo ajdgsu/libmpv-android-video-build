853b85c1f767fcd29d35e53f1a16f653fbfde758
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-11-26 17:34:31 +0800

    Add cache configuration for libxml2 build

--- buildscripts/scripts/libxml2.sh
+++ buildscripts/scripts/libxml2.sh
@@ -17,8 +17,14 @@ fi
 mkdir -p _build$ndk_suffix
 cd _build$ndk_suffix
 
+cat > android.cache <<EOF
+    ac_cv_func_pthread_create=yes
+    ac_cv_header_pthread_h=yes
+    EOF
+
 ../configure \
     CFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" CXXFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" \
+	-C android.cache \
 	--host=$ndk_triple \
     --disable-shared \
     --enable-static \
