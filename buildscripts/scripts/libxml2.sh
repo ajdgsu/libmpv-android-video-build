566457318b8dff327acecd746abc70d66523bbf8
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-11-26 18:36:26 +0800

    Update libxml2.sh

--- buildscripts/scripts/libxml2.sh
+++ buildscripts/scripts/libxml2.sh
@@ -17,13 +17,12 @@ fi
 mkdir -p _build$ndk_suffix
 cd _build$ndk_suffix
 
-cat > android.cache <<EOF
-    ac_cv_func_pthread_create=yes
-    ac_cv_header_pthread_h=yes
-    EOF
+printf "ac_cv_func_pthread_create=yes\nac_cv_header_pthread_h=yes\n" > android.cache
+
 
 ../configure \
     CFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" CXXFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" \
+	LIBS="-ldl" \
 	-C android.cache \
 	--host=$ndk_triple \
     --disable-shared \
