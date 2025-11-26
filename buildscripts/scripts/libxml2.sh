912d3cb68b75f4e2a35657e051dba2dec2bf6f2b
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-11-26 23:29:16 +0800

    Update libxml2.sh

--- buildscripts/scripts/libxml2.sh
+++ buildscripts/scripts/libxml2.sh
@@ -19,11 +19,11 @@ cd _build$ndk_suffix
 
 export ac_cv_header_sys_timeb_h=no
 
-../configure \
     CFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" CXXFLAGS="-O3 -mcpu=cortex-a725 -fno-plt -pipe -fvectorize -funroll-loops -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-ast-use-context -mllvm -polly-detect-keep-going -mllvm -polly-invariant-load-hoisting -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1 -mllvm -polly-run-dce -mllvm -hot-cold-split=true -flto=auto -fPIC" \
 	LIBS="-ldl" \
-	--build=x86_64-pc-linux-gnu \
+	../configure \
 	--host=aarch64-linux-android \
+	--build=x86_64-pc-linux-gnu \
     --disable-shared \
     --enable-static \
     --with-minimum \
